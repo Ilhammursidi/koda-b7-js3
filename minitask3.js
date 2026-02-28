7bfce8c090ec983d405f0d6f1753d5654f1f6633
Ilhammursidi <imursidi37@gmail.com>
2026-02-28 23:29:40 +0700

    Update minitask3.js

--- minitask3.js
+++ minitask3.js
@@ -4,15 +4,15 @@ let kembalian = totalUang - totalBeli;
 let nominal = [50000, 10000, 5000, 2000, 1000];
 
 if (kembalian < 0) {
-    console.log("Uang tidak cukup");
+  console.log("Uang tidak cukup");
 } else {
-    console.log("Kembalian: " + kembalian);
+  console.log("Kembalian: " + kembalian);
 }
 
 for (let i = 0; i < nominal.length; i++) {
-    let jumlahLembar = Math.floor(kembalian / nominal[i]);
-    if (jumlahLembar > 0) {
-        console.log(nominal[i] + " " +jumlahLembar + " lembar");
-        kembalian -= nominal[i] * jumlahLembar;
-    }
+  let jumlahLembar = Math.floor(kembalian / nominal[i]);
+  if (jumlahLembar > 0) {
+    console.log(nominal[i] + " " + jumlahLembar + " lembar");
+    kembalian -= nominal[i] * jumlahLembar;
+  }
 }
